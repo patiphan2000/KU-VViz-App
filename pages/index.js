ff403bf86b7a70620672f68e050dfb7dce4e3886
patiphan2000 <patiphan.sri@ku.th>
2023-04-01 14:35:21 +0700

    add empty array as course default data

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,9 @@ export default function Home() {
   return (
     <div style={{color: 'black'}}>
       <KuVViz
-      course = {data.course}
-      stdGrade={data.stdGrade}
-      stdEnroll={data.stdEnroll}
+      course = {data.course || []}
+      stdGrade={data.stdGrade || []}
+      stdEnroll={data.stdEnroll || []}
       />
     </div>
   )
