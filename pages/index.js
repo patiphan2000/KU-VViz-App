0ba0a9919af15ca12c1a7e5458dc46331f2ed451
patiphan2000 <patiphan.sri@ku.th>
2023-04-10 13:33:03 +0700

    show verification status & genEd subject

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,56 @@ export default function Home() {
     }
   }, [])
 
-  console.log(CourseVerification(
+  const verification = CourseVerification(
     data.course,
     data.stdGrade,
     data.stdEnroll,
     data.gened_and_others
-  ));
+  );
+  console.log(verification);
+
+  const listGenEd = [];
+
+  for (let group in verification.genEd) {
+    const currGroup = verification.genEd[group];
+    // console.log(verification.genEd[group].subject_list)
+    const subject_list = verification.genEd[group].subject_list;
+    const genGroup = (
+      <div style={{
+          fontFamily: 'Prompt',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '5px',
+          maxWidth: '60%'
+        }}>
+        <div key={group} style={{
+          fontFamily: 'Prompt',
+          display: 'flex',
+          justifyContent: 'space-between',
+          fontSize: '1.2em',
+          marginTop: '15px'
+        }}>
+          <div>{group}</div>
+          <div>{currGroup.credit_require + '/' + currGroup.credit_curr}</div>
+        </div>
+        <div>
+          {subject_list.map((e) => {
+            return (
+            <div style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}>
+              <div>{e.subject_code}</div>
+              <div>{e.subject_name_th}</div>
+              <div>{e.credit}</div>
+            </div>);
+          })}
+        </div>
+      </div>
+    )
+    listGenEd.push(genGroup);
+  }
 
   return (
     <div style={{color: 'black'}}>
@@ -34,7 +78,14 @@ export default function Home() {
       stdGrade={data.stdGrade || []}
       stdEnroll={data.stdEnroll || []}
       />
-      <div>{2}</div>
+      <div style={{fontFamily: 'Prompt', fontSize: '1.2em'}}>สถานะตรวจจบ: 
+        <span style={{
+          color: (verification.status)? 'green':'red'
+        }}>{(verification.status)? ' ผ่าน':' ไม่ผ่าน'}</span>
+      </div>
+      <div>{listGenEd.map((e) => {
+        return e;
+      })}</div>
     </div>
   )
 }
